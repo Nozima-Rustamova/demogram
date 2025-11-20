e7727c0bd44266bd7777a9e138ef59029b989e40
Your Name <you@example.com>
2025-11-20 21:36:41 +0500

    first commit

---
+++ demogram/demogram/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for demogram project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demogram.settings')
+
+application = get_asgi_application()
